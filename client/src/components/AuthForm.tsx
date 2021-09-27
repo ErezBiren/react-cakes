e1aa32c5db733da78e54e4c7e9f9a5abbced69ed
ErezBiren <biran330@gmail.com>
2021-09-27 17:47:31 +0300

    set clear icons

--- client/src/components/AuthForm.tsx
+++ client/src/components/AuthForm.tsx
@@ -1,3 +1,4 @@
+import { IconButton, InputAdornment } from "@material-ui/core";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -7,6 +8,13 @@ import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
+import {
+  Clear as ClearIcon,
+  Lock as LockIcon,
+  MailOutline as MailIcon,
+  Visibility,
+  VisibilityOff,
+} from "@material-ui/icons";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import React, { ChangeEvent, SyntheticEvent, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
@@ -44,6 +52,9 @@ const AuthForm = () => {
   const [data, setData] = useState({
     email: "",
     password: "",
+    showPassword: false,
+    emailFilled: false,
+    passwordFilled: false,
   });
 
   const dispatch = useDispatch();
@@ -51,15 +62,8 @@ const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
 
-  const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
-
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    console.log(data.email);
-    console.log(data.password);
 
     // optional: Add validation
 
@@ -116,7 +120,36 @@ const AuthForm = () => {
   };
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setData({ ...data, [e.target.name]: e.target.value });
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+      [e.target.name + "Filled"]: e.target.value,
+    });
+
+    console.log(data);
+  };
+
+  const handleClickShowPassword = () => {
+    setData({
+      ...data,
+      showPassword: !data.showPassword,
+    });
+  };
+
+  const ClearPass = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setData({
+      ...data,
+      password: "",
+      passwordFilled: false,
+    });
+  };
+
+  const ClearEmail = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setData({
+      ...data,
+      email: "",
+      emailFilled: false,
+    });
   };
 
   return (
@@ -132,6 +165,7 @@ const AuthForm = () => {
         </Typography>
 
         <TextField
+          value={data.email}
           variant="outlined"
           margin="normal"
           required
@@ -141,59 +175,80 @@ const AuthForm = () => {
           name="email"
           autoComplete="email"
           autoFocus
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <MailIcon />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment position="start">
+                {data.emailFilled && (
+                  <IconButton onClick={ClearEmail}>
+                    <ClearIcon />
+                  </IconButton>
+                )}
+              </InputAdornment>
+            ),
+          }}
           onChange={handleChange}
         />
         <TextField
+          value={data.password}
           variant="outlined"
           margin="normal"
           required
           fullWidth
           name="password"
           label="סיסמא"
-          type="password"
+          type={data.showPassword ? "text" : "password"}
           id="password"
           autoComplete="current-password"
           onChange={handleChange}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <LockIcon />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment position="end">
+                {data.passwordFilled && (
+                  <IconButton onClick={ClearPass}>
+                    <ClearIcon />
+                  </IconButton>
+                )}
+
+                <IconButton onClick={handleClickShowPassword}>
+                  {data.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <Link href="#" align="left" display="block" variant="body2">
+          שכחתי סיסמא
+        </Link>
+
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="זכור אותי"
         />
 
         <div className={styles.actions}>
           <Button
             type="submit"
-            fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
           >
             {isLogin ? "כניסה" : "הרשמה"}
           </Button>
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <button
-            type="button"
-            className={styles.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? "הרשמה חדשה" : "כניסה עם משתמש קיים"}
-          </button>
-          <div className={styles.passLabels}>
-            <label htmlFor="password">סיסמא</label>
-            <Link href="#">שכחת סיסמא? </Link>
-          </div>
         </div>
-        <Grid container>
-          <Grid item xs>
-            <Link href="#" variant="body2">
-              Forgot password?
-            </Link>
-          </Grid>
-          <Grid item>
-            <Link href="#" variant="body2">
-              {"Don't have an account? Sign Up"}
-            </Link>
-          </Grid>
-        </Grid>
+        <Link href="#" variant="body2">
+          {"לא רשומים? להרשמה מהירה"}
+        </Link>
       </form>
       {/* <FacebookLogin
         appId="1088597931155576"
